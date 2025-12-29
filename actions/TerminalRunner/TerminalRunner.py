09bb927bdc936fefedaae9cfc3f37441b85baf86
Leanspeed <jaedenhillesheim@gmail.com>
2025-12-28 18:28:03 -0700

    Trying to fit working plugin.

--- actions/TerminalRunner/TerminalRunner.py
+++ actions/TerminalRunner/TerminalRunner.py
@@ -1,5 +1,5 @@
 # Import StreamController modules
-from src.backend.PluginManager.ActionBase import ActionBase
+from src.backend.PluginManager.ActionCore import ActionCore
 from src.backend.DeckManagement.DeckController import DeckController
 from src.backend.PageManagement.Page import Page
 from src.backend.PluginManager.PluginBase import PluginBase
@@ -13,16 +13,25 @@ gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw
 
-class SimpleAction(ActionBase):
+# Logging
+from loguru import logger as log
+
+class TerminalRunner(ActionCore):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.has_configuration = True
+
+        self.counter: int = 0
         
     def on_ready(self) -> None:
-        icon_path = os.path.join(self.plugin_base.PATH, "assets", "info.png")
-        self.set_media(media_path=icon_path, size=0.75)
+        log.info("My action is ready")
+        self.set_center_label(str(self.counter))
         
     def on_key_down(self) -> None:
-        print("Key down")
+        self.counter += 1
+        log.info(f"Counter incremented to {self.counter}")
+        self.set_center_label(str(self.counter))
     
-    def on_key_up(self) -> None:
-        print("Key up")
+    # def on_key_up(self) -> None:
+    #     print("Key up")
--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 # Import StreamController modules
 from src.backend.PluginManager.PluginBase import PluginBase
 from src.backend.PluginManager.ActionHolder import ActionHolder
+from src.backend.PluginManager.ActionInputSupport import ActionInputSupport
 
 # Import actions
-from .actions.SimpleAction.SimpleAction import SimpleAction
+from .actions.TerminalRunner.TerminalRunner import TerminalRunner
 
 class PluginTemplate(PluginBase):
     def __init__(self):
         super().__init__()
 
         ## Register actions
-        self.simple_action_holder = ActionHolder(
+        self.terminal_runner_action_holder = ActionHolder(
             plugin_base = self,
-            action_base = SimpleAction,
-            action_id = "dev_core447_Template::SimpleAction", # Change this to your own plugin id
-            action_name = "Simple Action",
+            action_core = TerminalRunner,
+            action_id_suffix = "TerminalRunner", # Change this to your own plugin id
+            action_name = "Terminal Runner",
+            action_support = {
+                Input.Key: ActionInputSupport.SUPPORTED,
+                Input.Dial: ActionInputSupport.UNTESTED,
+                Input.Touchscreen: ActionInputSupport.UNTESTED
+            }
         )
-        self.add_action_holder(self.simple_action_holder)
+        self.add_action_holder(self.terminal_runner_action_holder)
 
         # Register plugin
         self.register(
-            plugin_name = "Template",
-            github_repo = "https://github.com/StreamController/PluginTemplate",
+            plugin_name = "Terminal Runner",
+            github_repo = "https://github.com/Leanspeed/StreamController-TerminalRunner",
             plugin_version = "1.0.0",
             app_version = "1.1.1-alpha"
         )
